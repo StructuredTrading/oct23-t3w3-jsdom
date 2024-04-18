c94f69d782e07aa45cdebd625dcea6b5ac8bab9f
StructuredTrading <samowen1992@hotmail.com>
2024-04-18 19:40:16 +1000

    feat: Toggle CSS function implimented.

--- favouriteAnimals.js
+++ favouriteAnimals.js
@@ -47,7 +47,7 @@ function createAnimalList(){
 function removeAnimalFromList(targetAnimalId){
     // 1. Find element in the list with matching ID
     let targetListItem = document.getElementById(targetAnimalId);
-    targetListItem = document.getElementsByClassName
+    // targetListItem = document.getElementsByClassName
     targetListItem.remove();
 
     // 2. Check if ID is in array of animals
